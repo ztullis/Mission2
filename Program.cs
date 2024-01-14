20a238a7c5aeb2b63f9e026d5a6d5e43de31c073
zachjtullis <zachjtullis@ZACHARYTULL3320>
2024-01-14 01:05:32 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Mission2;
+using System.Diagnostics.Tracing;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        rollDice rolledDice = new rollDice();
+
+        //Initializing the number of rolls at 0.
+        int numRolls;
+        int[] eachRollCount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        System.Console.WriteLine("\nHow many times will the dice be thrown?");
+        string userInput = System.Console.ReadLine();
+        if (userInput != null)
+        {
+            numRolls = int.Parse(userInput);
+        }
+        else
+        {
+            // Handle the case where input is null (e.g., user closed the console window)
+            // You might want to provide a default value or exit the program in a controlled manner.
+            System.Console.WriteLine("\nInvalid input. Exiting the program.");
+            return;
+        }
+
+        int[] rollTotals = rolledDice.calculateRolls(numRolls);
+
+        for (int iCount = 0; iCount < numRolls; iCount++)
+        {
+            int roll = rollTotals[iCount];
+
+            eachRollCount[roll - 2] += 1;
+        }
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
+
+        for (int iCount = 0; iCount < eachRollCount.Length; iCount++)
+        {
+            float percent = (((float)eachRollCount[iCount]/numRolls) * 100);
+            int asterisksCount = (int)percent;
+
+            string asterisks = new string('*', asterisksCount);
+
+            System.Console.WriteLine((iCount + 2) + ": " + asterisks);
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
---
+++ rollDice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class rollDice
+    {
+        public int[] calculateRolls(int numRolls)
+        {
+            Random random = new Random();
+
+            int[] rollTotals = new int[numRolls];
+
+            for (int iCount = 0; iCount < numRolls; iCount++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                int diceTotal = dice1 + dice2;
+
+                rollTotals[iCount] = diceTotal;
+            }
+
+            return rollTotals;
+        }
+    }
+}
