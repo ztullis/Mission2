46563f6f30748fec24071908db87477b9950bbe7
Zach Tullis <zachjtullis@gmail.com>
2024-01-17 14:11:04 -0700

    Last minute changes and comments

--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
 ﻿using Mission2;
 using System.Diagnostics.Tracing;
 
+//Class that takes a number of times a dice is rolled and prints out a histogram of how often (percentage) each number was rolled.
 public class Program
 {
+    //main method that runs in this class automatically
     private static void Main(string[] args)
     {
+        //Instantiates the object from the rollDice class
         rollDice rolledDice = new rollDice();
 
-        //Initializing the number of rolls at 0.
+        //Initializing the number of rolls at 0, and creates the numRolls variable
         int numRolls;
         int[] eachRollCount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+        //Welcomes the user to the game and asks them how many times they want to roll the dice, then sets their answer to the userInput variable
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
-
         System.Console.Write("\nHow many times will the dice be thrown? ");
         string userInput = System.Console.ReadLine();
+
+        //If the user's input is null, it will run this error message and quit the game. 
         if (userInput != null)
         {
             numRolls = int.Parse(userInput);
@@ -27,8 +32,10 @@ public class Program
             return;
         }
 
+        //Initializes the rolledTotals integer list and runs the calculateRolls method in the rollDice class (other page)
         int[] rollTotals = rolledDice.calculateRolls(numRolls);
 
+        //For each of the returned values of the returned values rolled, add a count of 1 for each time that number was rolled.
         for (int iCount = 0; iCount < numRolls; iCount++)
         {
             int roll = rollTotals[iCount];
@@ -36,10 +43,12 @@ public class Program
             eachRollCount[roll - 2] += 1;
         }
 
+        //Return the number of rolls and the explanation of the results
         System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
         System.Console.WriteLine("Total number of rolls = " + numRolls + ".\n");
 
+        //For each possible number rolled, calculate the percentage of times that number was rolled, round it to the nearest whole number, and print an asterisk for each percent for that number. 
         for (int iCount = 0; iCount < eachRollCount.Length; iCount++)
         {
             float percent = (((float)eachRollCount[iCount]/numRolls) * 100);
@@ -50,6 +59,7 @@ public class Program
             System.Console.WriteLine((iCount + 2) + ": " + asterisks);
         }
 
+        //Print a thank you message
         System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
     }
 }
--- rollDice.cs
+++ rollDice.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//This is in the same namespace as the Program.cs class
 namespace Mission2
 {
+    //rollDice method that rolls each dice and puts the numbers rolled into an array (that is passed back to the Program.cs)
     internal class rollDice
     {
+        //Method that rolls each dice and puts the numbers rolled into an array
         public int[] calculateRolls(int numRolls)
         {
+            //Get the random object into my code
             Random random = new Random();
 
+            //Setting the rollTotals variable equal to an array with the size of the number of rolls
             int[] rollTotals = new int[numRolls];
 
+            //For loop randomly rolling the dice (1 to 6), adding them up, and adding the sum to the rollTotals array.
             for (int iCount = 0; iCount < numRolls; iCount++)
             {
                 int dice1 = random.Next(1, 7);
@@ -24,6 +29,7 @@ namespace Mission2
                 rollTotals[iCount] = diceTotal;
             }
 
+            //Then the rollTotals array is sent back to the Program.cs
             return rollTotals;
         }
     }
